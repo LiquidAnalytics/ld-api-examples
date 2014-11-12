37703e94dee070c8025bd8ee5430e0c41b6db541
Venkatesh Vengaldas <vvengaldas@deloitte.com>
2014-11-12 08:10:46 -0600

    For stress testing of cart

--- nodejs/src/rapidCart.js
+++ nodejs/src/rapidCart.js
@@ -380,7 +380,7 @@ var poll = function (host, accessToken, deliveryConfirmations, username, state, 
 
 // Pass in arguments
 var PassInCommand = function(args){
-    runTest(args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8]);
+    runTest(args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9],args[10]);
 }
 
 PassInCommand(process.argv.slice(2));
